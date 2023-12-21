4b43f995d87ee0032effa0d86b3cb62c53a917e1
smhrd <daehodr@gmail.com>
2023-12-21 17:02:58 +0900

    gg

---
+++ app/src/main/java/com/example/faindsapplication/PwActivity.java
@@ -0,0 +1,14 @@
+package com.example.faindsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PwActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pw);
+    }
+}
