801f58aebcf87d029a5942f74e8b957cd53bc37b
jjj <jaeyoung_98@naver.com>
2023-12-22 12:10:10 +0900

    jjj

--- app/src/main/java/com/example/faindsapplication/LoginActivity.java
+++ app/src/main/java/com/example/faindsapplication/LoginActivity.java
@@ -4,8 +4,11 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -33,25 +36,36 @@ public class LoginActivity extends AppCompatActivity {
             queue = Volley.newRequestQueue(this);
         }
 
-        //아이디, 비빈 입력 받고 일치하면 MainActivity 이동
+        //로그인 기능
         binding.loginBtnLogin.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 StringRequest request = new StringRequest(
                         Request.Method.POST,
-                        "http://localhost:8089/login",
+                        "http://3.145.203.141:27017/login",
                         new Response.Listener<String>() {
                             @Override
                             public void onResponse(String response) {
-
+                                Log.d("LoginActivity", response);
+                                // 응답이 "true"이면 로그인 성공, "false"이면 실패
+                                if (response.equals("true")) {
+                                    String id = binding.loginIdHint.getText().toString();
+                                    // 로그인 성공
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    saveUserId(id);
+                                    startActivity(intent);
+                                } else {
+                                    // 로그인 실패
+                                    Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+                                }
                             }
                         }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-
                     }
-                }){
+                }
+
+                ){
                     @Nullable
                     @Override
                     protected Map<String, String> getParams() throws AuthFailureError {
@@ -60,22 +74,30 @@ public class LoginActivity extends AppCompatActivity {
                         Map<String,String> params = new HashMap<>();
                         params.put("id",id);
                         params.put("pw",pw);
+                        //Spring서버에서도 "id","pw"로 받아야 함
                         return params;
                     }
                 };
+
                 queue.add(request);
+
             }
         });
-        Intent intent =new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(intent);
-        //회원가입 버튼 누르면 회원가입 JoinActivity 이동
+        // 회원가입 기능
         binding.loginTvJoin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent(LoginActivity.this, JoinActivity.class);
+                Intent intent = new Intent(LoginActivity.this,JoinActivity.class);
                 startActivity(intent);
             }
         });
-
+        }
+    public void saveUserId(String userId) {
+        // SharedPreferences 인스턴스 얻기
+        SharedPreferences sharedPreferences = getSharedPreferences("MyAppPreferences", MODE_PRIVATE);
+        // Editor를 사용하여 값을 저장
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("UserID", userId);
+        editor.apply();
     }
 }
