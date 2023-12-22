10e3b6d85d54442aeddb86d20dc18df6ae64a1e6
hw-Shin <yh3451@gmail.com>
2023-12-22 14:23:40 +0900

    12/22 오후 카메라 연결만

--- app/src/main/java/com/example/faindsapplication/Register/RegisterItemClickListener.java
+++ app/src/main/java/com/example/faindsapplication/Register/RegisterItemClickListener.java
@@ -1,4 +1,7 @@
 package com.example.faindsapplication.Register;
 
+import android.view.View;
+
 public interface RegisterItemClickListener {
+    void onItemClickListener(View v, int position);
 }
--- app/src/main/java/com/example/faindsapplication/RegisterDetail/RegisterDetailActivity.java
+++ app/src/main/java/com/example/faindsapplication/RegisterDetail/RegisterDetailActivity.java
@@ -2,15 +2,22 @@ package com.example.faindsapplication.RegisterDetail;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.faindsapplication.R;
+import com.example.faindsapplication.databinding.ActivityRegisterDetailBinding;
 
 public class RegisterDetailActivity extends AppCompatActivity {
-
+    private ActivityRegisterDetailBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register_detail);
+        binding = ActivityRegisterDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intent = getIntent();
+        //int imgTest = intent.getIntExtra("TestImg",R);
+        //binding.imgTest.setImageResource(imgTest);
     }
 }
