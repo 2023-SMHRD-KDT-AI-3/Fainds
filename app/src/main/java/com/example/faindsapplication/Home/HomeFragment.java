cb3d605f6668f537c378bece5ac8c7ace1672161
jjj <jaeyoung_98@naver.com>
2023-12-22 16:37:09 +0900

    jjj

--- app/src/main/java/com/example/faindsapplication/Home/HomeFragment.java
+++ app/src/main/java/com/example/faindsapplication/Home/HomeFragment.java
@@ -1,5 +1,10 @@
 package com.example.faindsapplication.Home;
 
+
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,17 +12,36 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.faindsapplication.R;
 
 
 public class HomeFragment extends Fragment {
 
-    
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+
+       // TextView name = view.findViewById(R.id.tvUserName);
+
+       // name.setText(getUserId());
+
+
+
+
+
+
+        return view;
     }
+    public String getUserId() {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("MyAppPreferences", MODE_PRIVATE);
+        // "UserID" 키로 저장된 값을 반환. 값이 없다면 null 반환
+        return sharedPreferences.getString("UserID", null);
+    }
+
+
 }
