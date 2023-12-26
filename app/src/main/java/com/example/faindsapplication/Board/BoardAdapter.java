654bf4443f961a143a799dbb29d564066186c1c1
jjj <jaeyoung_98@naver.com>
2023-12-26 14:39:48 +0900

    jjj

--- app/src/main/java/com/example/faindsapplication/Board/BoardAdapter.java
+++ app/src/main/java/com/example/faindsapplication/Board/BoardAdapter.java
@@ -1,6 +1,5 @@
 package com.example.faindsapplication.Board;
 
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,17 +9,20 @@ import androidx.recyclerview.widget.RecyclerView;
 
 
 import com.example.faindsapplication.R;
-import com.example.faindsapplication.Register.RegisterViewHolder;
-import com.example.faindsapplication.databinding.ActivityBoardDetailBinding;
 
 import java.util.ArrayList;
 
 public class BoardAdapter extends RecyclerView.Adapter<BoardViewHolder> {
 
     private ArrayList<BoardVO> dataset;
+
+    private ArrayList<String> keyset;
+
     public BoardAdapter(ArrayList<BoardVO> dataset) {
         this.dataset = dataset;
+        this.keyset = keyset;
     }
+
     @NonNull
     @Override
     public BoardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -31,17 +33,19 @@ public class BoardAdapter extends RecyclerView.Adapter<BoardViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull BoardViewHolder holder, int position) {
+
+
         String boardTitle = dataset.get(position).getBoardTitle();
         String boardContent = dataset.get(position).getBoardContent();
-        String boardCmtNum = dataset.get(position).getBoardCmtNum();
+       // int boardCmtNum = dataset.get(position).getBoardCmtNum();
         holder.getBoardTitle().setText(boardTitle);
         holder.getBoardContent().setText(boardContent);
-        holder.getBoardCmtNum().setText(boardCmtNum);
+      //  holder.getBoardCmtNum().setText(String.valueOf(boardCmtNum));
         holder.listener = new BoardItemClickListener() {
             @Override
             public void onItemClickListener(View v, int position) {
-                //Intent intent = new Intent(v.getContext());
-                //v.getContext().startActivity(intent);
+               // Intent intent = new Intent(v.getContext(),);
+              //  v.getContext().startActivity(intent);
             }
         };
 
--- app/src/main/java/com/example/faindsapplication/Setting/SettingFragment.java
+++ app/src/main/java/com/example/faindsapplication/Setting/SettingFragment.java
@@ -1,5 +1,9 @@
 package com.example.faindsapplication.Setting;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,16 +12,42 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.faindsapplication.LoginActivity;
 import com.example.faindsapplication.R;
+import com.example.faindsapplication.databinding.FragmentBoardBinding;
+import com.example.faindsapplication.databinding.FragmentSettingBinding;
 
 
 public class SettingFragment extends Fragment {
 
+    private FragmentSettingBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+
+        binding = FragmentSettingBinding.inflate(inflater,container,false);
+
+        binding.settingLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            removeUserId();
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            startActivity(intent);
+            getActivity().finish();
+            }
+        });
+
+
+
+
+        return binding.getRoot();
+    }
+    public void removeUserId() {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("MyAppPreferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.remove("UserID");
+        editor.apply();
     }
 }
