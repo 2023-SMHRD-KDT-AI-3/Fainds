cbf551edeadbc36b436074d6fbfbd02bbbb98cb1
jjj <jaeyoung_98@naver.com>
2023-12-27 10:26:06 +0900

    jjj

--- app/src/main/java/com/example/faindsapplication/Board/BoardAdapter.java
+++ app/src/main/java/com/example/faindsapplication/Board/BoardAdapter.java
@@ -1,6 +1,7 @@
 package com.example.faindsapplication.Board;
 
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +14,9 @@ import com.example.faindsapplication.R;
 import com.example.faindsapplication.Register.RegisterViewHolder;
 import com.example.faindsapplication.databinding.ActivityBoardDetailBinding;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class BoardAdapter extends RecyclerView.Adapter<BoardViewHolder> {
 
@@ -34,7 +37,16 @@ public class BoardAdapter extends RecyclerView.Adapter<BoardViewHolder> {
         String boardTitle = dataset.get(position).getBoardTitle();
         String boardContent = dataset.get(position).getBoardContent();
         int boardCmtNum = dataset.get(position).getBoardCmtNum();
-        String createdAt = dataset.get(position).getCreated_at().substring(0,10) +" " +dataset.get(position).getCreated_at().substring(12,16);
+         String createdAt = dataset.get(position).getCreated_at().substring(0,10) +" " +dataset.get(position).getCreated_at().substring(11,16);
+       // String createdAt = dataset.get(position).getCreated_at();
+
+        long now =System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        String getTime = sdf.format(date);
+        Log.d("gettime",getTime);
+        Log.d("gettime",createdAt);
+
         int boardSeq=dataset.get(position).getBoardSeq();
         holder.getBoardTitle().setText(boardTitle);
         holder.getBoardContent().setText(boardContent);
